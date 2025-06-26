d302b14b7fdcb8746efbd2311b2bef4d7b42330c
Noah Kvirkvelia <kvirkvelianoah@gmail.com>
2025-06-26 23:45:56 +0400

    fn currency converter

--- app.js
+++ app.js
@@ -21,4 +21,34 @@ console.log(emptyName("   "));
 console.log(emptyName("1992/04"));
 console.log(emptyName("Noah Fisher"));
 
-// 
+// 2. dollar to gel converter
+function convert() {
+    const dollar = 5;
+    const currencyRate = 2.74;
+    const gel = dollar * currencyRate;
+    return `${dollar} dollars is equal to ${gel} GEL.`;
+}
+console.log(convert());
+
+// 3. currency converter
+function convertCurrency() {
+    const amount = parseFloat(document.getElementById("currencyInput").value);
+    const selectedCurrency = document.getElementById("currencySelect").value;
+    let currencyRate;
+
+    switch (selectedCurrency) {
+        case "USD":
+            currencyRate = 2.7210; 
+            break;
+        case "EUR":
+            currencyRate = 3.1800; 
+            break;
+        case "GBP":
+            currencyRate = 3.7063; 
+            break;
+        default:
+            currencyRate = "unknown currency"; 
+    }
+    const convertedAmount = amount * currencyRate;
+    document.getElementById("currencyResult").textContent = `converted amount in GEL: ${convertedAmount}`;
+}

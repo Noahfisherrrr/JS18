b94e65acb1c303f2d6d4533ab5d44c6f7ada0ecb
Noah Kvirkvelia <kvirkvelianoah@gmail.com>
2025-06-26 22:46:37 +0400

    fn check name

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+// 1. function to check if name is empty
 
+function emptyName(input) {
+    return !input || input.trim() === "";;
+}
+function checkName() {
+    const inputField = document.getElementById("nameInput");
+    const name = inputField.value;
+
+let result = emptyName(name)
+    if (emptyName(name)) {
+    result = "Name is empty.";
+    } else {
+    result = "Name is valid.";
+    }
+
+document.getElementById("result").textContent = result;
+}
+console.log(emptyName(""));
+console.log(emptyName("   "));
+console.log(emptyName("1992/04"));
+console.log(emptyName("Noah Fisher"));
+
+// 
